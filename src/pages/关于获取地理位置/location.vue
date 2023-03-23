10c61aaa5d689448ccc5e4ca4be9dda88dddf212
Siege lion <405843463@qq.com>
2023-03-23 19:33:53 +0800

    uniapp 获取地理位置

---
+++ src/pages/关于获取地理位置/location.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import AMapWX from '@/common/amap-wx.js'
+const AampWX = ref('这里是高德开放平台获得的key')
+const amapPlugin = ref()
+const popupShow = ref(false)
+onLoad(() => {
+  amapPlugin.value = new AMapWX({
+    key: AampWX.value,
+  })
+  getRegeo()
+  getWeather()
+})
+const getRegeo = () => {
+  amapPlugin.value.getRegeo({
+    success: (data: any) => {
+      console.log(data)
+    },
+  })
+}
+const getWeather = () => {
+  amapPlugin.value.getWeather({
+    success: (data: any) => {
+      console.log(data)
+    },
+  })
+}
+const handleChangeLocal = () => {
+  popupShow.value = true
+}
+</script>
